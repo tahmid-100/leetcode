27cdadcf86b48d2c4a81a88f4c6bf62f916a75cb
Kh. Tahmid Alam <157390459+tahmid-100@users.noreply.github.com>
2026-03-06 22:53:00 +0600

    Time: 12 ms (92.8%), Space: 61 MB (62.82%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,22 +1,48 @@
 class MyHashMap {
 public:
-    int data[1000001];
-    MyHashMap() {
-        for(int i=0;i<1000001;i++){
-            data[i]=-1;
-        }
+    vector<list<pair<int,int>>>table;
+    MyHashMap():table(1000) {
+         
+    }
+
+    int hash(int key){
+        return key % table.size();
     }
     
     void put(int key, int value) {
-         data[key]=value;
+        int h=hash(key);
+
+        for(auto& x : table[h]){
+            if(x.first==key){
+                x.second=value;
+                return;
+
+            }
+        }
+
+        table[h].push_back({key,value});
     }
     
     int get(int key) {
-        return data[key];
+       int h=hash(key);
+
+       for(auto it:table[h]){
+           if(it.first==key) return it.second;
+       }
+
+       return -1;
     }
     
     void remove(int key) {
-        data[key]=-1;
+         int h=hash(key);
+
+        auto& bucket = table[h];
+        for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+            if (it->first == key) {
+                bucket.erase(it);
+                return;  
+            }
+        }
     }
 };
 
