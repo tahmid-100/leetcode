2c9a5ad094c0a544d476b1ceac99d6c74eec5084
Kh. Tahmid Alam <157390459+tahmid-100@users.noreply.github.com>
2026-04-28 22:34:56 +0600

    Time: 0 ms (100%), Space: 8.6 MB (32.26%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+
+        if(s.size()>t.size()) return false;
+
+        int i=0;
+
+        int j=0;
+
+        while(i<t.size() && j<s.size()){
+
+            if(t[i]==s[j]){
+                j++;
+            
+            }
+
+             i++;
+        }
+           
+        if(j==s.size()) return true;
+
+        return false;
+        
+    }
+};
